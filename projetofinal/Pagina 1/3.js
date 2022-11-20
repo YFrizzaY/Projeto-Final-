033eabb9a1527b13b012b90c05b6b545e8fa2c26
Paulo Henrique <104027031+YFrizzaY@users.noreply.github.com>
2022-11-20 08:32:05 -0300

    Add files via upload

---
+++ projetofinal/Pagina 1/3.js
@@ -0,0 +1,56 @@
+var header           = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var content          = document.getElementById('content');
+var showSidebar      = false;
+
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;
+    if(showSidebar)
+    {
+        navigationHeader.style.marginLeft = '-10vw';
+        navigationHeader.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(2px)';
+    }
+    else
+    {
+        navigationHeader.style.marginLeft = '-100vw';
+        navigationHeader.style.animationName = '';
+        content.style.filter = '';
+    }
+}
+
+function closeSidebar()
+{
+    if(showSidebar)
+    {
+        showSidebar = true;
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar) 
+    {  
+        showSidebar = true;
+        toggleSidebar();
+    }
+});
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrosel() {
+	idx++;
+
+	if (idx > img.length - 1) {
+		idx = 0;
+	}
+
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+setInterval(carrosel, 1800);
+
